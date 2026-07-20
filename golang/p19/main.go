802f7eb47a801b8ca9f6d8f14dff9ee4a0b1afe7
Hassan <62506881+hasan-yousefi@users.noreply.github.com>
2026-07-20 21:24:45 +0330

    adding another example interface

--- golang/p19/main.go
+++ golang/p19/main.go
@@ -1,1 +1,33 @@
-package p19
+package main
+
+import "fmt"
+
+type Animal interface {
+	speak(word string)
+}
+
+type Dog struct {
+	name string
+}
+type Cat struct {
+	name string
+}
+
+func (d Dog) speak(word string) {
+	fmt.Printf("%s says: %s\n", d.name, word)
+}
+
+func (c Cat) speak(word string) {
+	fmt.Printf("%s says: %s\n", c.name, word)
+}
+
+func Makespeak(animal Animal, word string) {
+	animal.speak(word)
+}
+
+func main() {
+	dog := Dog{name: "Buddy"}
+	cat := Cat{name: "Whiskers"}
+	Makespeak(dog, "Woof!")
+	Makespeak(cat, "Meow!")
+}
